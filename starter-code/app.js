4255fa495095e2ea087f51daac3363f37b0eb250
mateszucs96 <szucsmate096@gmail.com>
2022-03-27 18:57:54 +0200

    add logic to handle if someone won the game

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,4 +1,3 @@
-console.log(document.querySelectorAll('.player-container'))
 const gridContainer = document.querySelector('.grid-container');
 
 let grid;
@@ -7,7 +6,7 @@ let secondaryMark;
 let player1;
 let player2;
 let players;
-
+let gameArr = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 class Player {
     constructor(mark, canMove) {
@@ -16,20 +15,26 @@ class Player {
         grid.forEach(el => el.addEventListener('click', this.placeMarks))
     }
     placeMarks(e) {
-        console.log(primaryMark)
-        if (e.target.children.length === 0) {
 
-            console.log(e.target.children)
+        if (e.target.children.length === 0 && !newGame.checkForWin()) {
+
+
             const playerMark = document.createElement('img');
             playerMark.classList.add('marks');
             playerMark.classList.add('player-mark');
             players.forEach(player => {
                 if (player.canMove) {
+                    const idx = +e.target.dataset.index
                     playerMark.src = player.mark
-                    e.target.appendChild(playerMark);
+                    e.target.appendChild(playerMark)
+                    gameArr[idx] = player.mark === './assets/icon-x.svg' ? 'x' : 'o';
+                    console.log(gameArr)
                 }
             })
+
+            console.log(gameArr)
             newGame.handleTurns();
+            newGame.checkForWin();
         }
     }
 }
@@ -51,12 +56,12 @@ class Game {
     checkFigure() {
         this.figure.forEach(el => {
             if (el.classList.contains('active')) {
-                // primaryMark = el.children.getAttribute('src');
+
                 primaryMark = el.children[0].getAttribute('src')
-                console.log(primaryMark, 'primary')
+
             } else {
                 secondaryMark = el.children[0].getAttribute('src');
-                console.log(secondaryMark, 'secondary')
+
             }
         })
     }
@@ -86,6 +91,8 @@ class Game {
         const html = `<div class="grid"></div>`;
         for (let i = 0; i < this.boardCount; i++) {
             gridContainer.insertAdjacentHTML('afterbegin', html)
+            const singleGrid = document.querySelector('.grid')
+            singleGrid.dataset.index = i;
         }
         grid = document.querySelectorAll('.grid')
     }
@@ -93,13 +100,81 @@ class Game {
         this.loadBoard();
         players = [player1 = new Player(primaryMark, primaryMark === './assets/icon-x.svg' ? true : false), player2 = new Player(secondaryMark, secondaryMark === './assets/icon-x.svg' ? true : false)]
 
-
-
     }
 
     handleTurns() {
         players.forEach(player => player.canMove ? player.canMove = false : player.canMove = true)
-        console.log(players)
+
+    };
+    loop() {
+        //loop horizontal       
+        for (let i = 0; i < gameArr.length; i += 3) {
+            let arr = []
+
+            for (let k = i; k < i + 3; k++) {
+                arr.push(gameArr[k])
+                console.log(arr)
+            }
+            if (arr.every(el => el === 'x')) {
+                console.log(`x won`);
+                return 'x won'
+            }
+            if (arr.every(el => el === 'o')) {
+                console.log(`o won`);
+                return 'o won'
+            }
+        }
+        //loop vertical        
+        for (let i = 0; i < 3; i++) {
+            let arr = []
+
+            for (let k = i; k < i + 6 + 1; k += 3) {
+                arr.push(gameArr[k])
+                console.log(arr)
+            }
+            if (arr.every(el => el === 'x')) {
+                console.log(`x won`);
+                return 'x won'
+            }
+            if (arr.every(el => el === 'o')) {
+                console.log(`o won`);
+                return 'o won'
+            }
+        }
+        //loop diagonal(cross to left)
+        let arrLeft = []
+        for (let i = 0; i < gameArr.length + 1; i += 4) {
+
+            arrLeft.push(gameArr[i])
+            console.log(arrLeft)
+        }
+        if (arrLeft.every(el => el === 'x')) {
+            console.log(`x won`);
+            return 'x won'
+        }
+        if (arrLeft.every(el => el === 'o')) {
+            console.log(`o won`);
+            return 'o won'
+        }
+        //loop diagonal(cross to right)
+        let arrRight = []
+        for (let i = 2; i < 7; i += 2) {
+
+            arrRight.push(gameArr[i])
+            console.log(arrRight)
+        }
+        if (arrRight.every(el => el === 'x')) {
+            console.log(`x won`);
+            return 'x won'
+        }
+        if (arrRight.every(el => el === 'o')) {
+            console.log(`o won`);
+            return 'o won'
+        }
+    }
+
+    checkForWin() {
+        return this.loop();
     }
 }
 
